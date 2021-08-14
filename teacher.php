b4333c20f7b83bb077e4fe0f4c88cd0be1cf9a7f
k .Hemant <38524840+k-hemant@users.noreply.github.com>
2021-08-15 05:27:17 +0530

    Update teacher.php

--- teacher.php
+++ teacher.php
@@ -138,22 +138,7 @@ $con->close();
 else{}
 ?>
 
-<script>
 
-var front = false;
-var video = document.querySelector('video');
-  document.getElementById('flipCamera').onclick = function() { front = !front; };
-  var constraints = { video: { facingMode: (front? "user" : "environment"), width: 640, height: 480  } };
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(function(mediaStream) {
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = function(e) {
-    video.play();
-};
-})
-.catch(function(err) { console.log(err.name + ": " + err.message); })
-
-</script>
 
 
 </html>
